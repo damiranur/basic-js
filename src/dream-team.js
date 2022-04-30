91bdf5af82f00575c64b263289011365cfb5bf21
Damira <damira.cyprus@gmail.com>
2022-04-30 11:51:47 +0300

    feat: solved tasks count-cats and dream-teams

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,37 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const onlyNames = members.filter((name) => {
+    if (typeof name === 'string') {
+      return typeof name[0] === 'string';
+    }
+    return;
+  });
+  const firstLetters = onlyNames
+    .map((name) => {
+      let trimmedName = name.trim();
+      return trimmedName[0].toUpperCase();
+    })
+    .sort()
+    .join('');
+  return firstLetters;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
