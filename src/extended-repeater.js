b20ff0a2bef1e78536f10af4e243d7275cf898de
Damira <damira.cyprus@gmail.com>
2022-05-02 18:54:32 +0300

    feat: solved tasks extended-repeater, hanoi, recursive

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,61 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let arrRepeatStr = [];
+  let arrRepeatAddition = [];
+  let additionStr = '';
+  let resultStr = '';
+
+  if (options.repeatTimes != undefined) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      arrRepeatStr.push(String(str));
+    }
+  } else {
+    arrRepeatStr.push(String(str));
+  }
+
+  if (options.addition !== undefined) {
+    if (
+      options.additionRepeatTimes === undefined ||
+      options.additionRepeatTimes === 0
+    ) {
+      arrRepeatAddition.push(String(options.addition));
+    } else {
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        arrRepeatAddition.push(String(options.addition));
+      }
+    }
+  }
+
+  if (options.additionSeparator === undefined) {
+    additionStr = arrRepeatAddition.join('|');
+  } else {
+    additionStr = arrRepeatAddition.join(options.additionSeparator);
+  }
+
+  arrRepeatStr = arrRepeatStr.map((item) => item + additionStr);
+  if (options.separator === undefined) {
+    resultStr = arrRepeatStr.join('+');
+  } else {
+    resultStr = arrRepeatStr.join(options.separator);
+  }
+  return resultStr;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
